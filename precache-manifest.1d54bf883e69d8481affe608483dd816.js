19a4b20f112ae515e7a79144e10d76563cefc215
Sorin Pavelescu <paveless@uni.coventry.ac.uk>
2020-03-22 15:15:58 +0000

    Updates

---
+++ precache-manifest.1d54bf883e69d8481affe608483dd816.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f08eea9f1aaeec5f7466d9eb28372e",
+    "url": "/monsters-pokedex/index.html"
+  },
+  {
+    "revision": "033dd44d54deaa6fd5d4",
+    "url": "/monsters-pokedex/static/css/main.634d58c8.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-pokedex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-pokedex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033dd44d54deaa6fd5d4",
+    "url": "/monsters-pokedex/static/js/main.b57a29e4.chunk.js"
+  },
+  {
+    "revision": "17ed6aadcb0ef9250023",
+    "url": "/monsters-pokedex/static/js/runtime-main.666d0e2c.js"
+  }
+]);
